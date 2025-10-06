f247f6caff47daa960f1565d68c21881e8068ad5
bigbadben <dovidcreeger@gmail.com>
2025-10-06 01:48:26 -0400

    more changes to fix macos test running in github

---
+++ ios/DerivedData/Build/Intermediates.noindex/ListIt.build/Debug-iphonesimulator/ProfileFeature.build/DerivedSources/ProfileFeature_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ProfileFeatureVersionString[];
+ extern const double ProfileFeatureVersionNumber;
+
+ const unsigned char ProfileFeatureVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ProfileFeature  PROJECT:ListIt-1" "\n";
+ const double ProfileFeatureVersionNumber __attribute__ ((used)) = (double)1.;
