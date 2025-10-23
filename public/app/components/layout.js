11833218fc7aba9e8817a6a9bb25923cd35c04ed
Dovidc <dovidcreeger@gmail.com>
2025-10-23 01:07:25 -0400

    text

--- public/app/components/layout.js
+++ public/app/components/layout.js
@@ -163,7 +163,7 @@
         H('div', { className: 'container row', style: { justifyContent: 'space-between' } },
           H('div', { className: 'row', style: { gap: 18, alignItems: 'center' } },
             H('div', { className: 'brand-copy' },
-              H('div', { className: 'brand-title' }, 'Trovlr')
+              H('div', { className: 'brand-title' }, 'Trovelr')
             )
           ),
           H('nav', { className: 'row' },
