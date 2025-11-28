70c3adfd91fbe26ed293c0b5923f32f4fb32c733
Dovidc <dovidcreeger@gmail.com>
2025-11-28 12:54:59 -0500

    changes to keyboard hiding when search

--- public/app/app-shell.js
+++ public/app/app-shell.js
@@ -1049,6 +1049,7 @@
                                 onKeyDown: e => {
                                   if (e.key === 'Enter') {
                                     e.preventDefault();
+                                    e.target.blur();
                                     submitSearch();
                                   }
                                 },
